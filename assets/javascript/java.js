21739640544e27b1ba3a2ad2d296f15badb20b7b
rabidima <dmitrirabinovich@Dmitris-MacBook-Pro.local>
2016-08-13 12:02:03 -0700

    adding https for giphy.com address

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -25,7 +25,7 @@ $('.arrayButtons').on('click', function(){
 	// console.log('array', heroesNames);
 	console.log('hero name:',hero);
 
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=dc6zaTOxFJmzC&limit=10";
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 
 	$("#showDiv").empty();
